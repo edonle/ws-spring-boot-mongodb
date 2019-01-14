0f1084b34b4bebbc8b1e87f5cc26af31d3f71a99
Edson Carlos <edonle@gmail.com>
2019-01-14 11:55:35 -0200

    Primeiro commit - projeto criado

---
+++ src/test/java/com/example/wsmongo/WsmongoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.wsmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsmongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
